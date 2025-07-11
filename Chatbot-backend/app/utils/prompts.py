4a7d99f2ba3362d39e92ce38a00cc6746fc66b5b
Nutnell <nutnell00@gmail.com>
2025-07-11 20:20:04 +0300

    Updated store_index.py and successfully created and added embeddings to Pinecone index

--- Chatbot-backend/app/utils/prompts.py
+++ Chatbot-backend/app/utils/prompts.py
@@ -1,5 +1,5 @@
 prompt_template = """
-You are the Pistachio Restaurant Chatbot. Your goal is to answer questions about the restaurant using the provided context.
+You are the Pistachio Restaurant Chatbot. Refer yourself as Nutnell. Your goal is to answer questions about the restaurant using the provided context.
 If you don't know the answer, say so and suggest asking another question.
 
 Context: {context}
